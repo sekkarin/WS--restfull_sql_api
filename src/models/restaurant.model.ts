5fca7e569631af507297ef4d016352e587e930ea
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-09-11 11:20:13 +0700

    ..

--- src/models/restaurant.model.ts
+++ src/models/restaurant.model.ts
@@ -3,13 +3,13 @@ import { RestauantInterface } from '../types/Restauant'
 
 
 export const create = (newRestaurant: RestauantInterface) => sql.query(
-    `INSERT INTO se_database.Restaurant (id, name, type, imageUrl) VALUES (NULL,'${newRestaurant.name}','${newRestaurant.type}','${newRestaurant.imgUrl}')`,
+    `INSERT INTO 'Restaurant' (id, name, type, imageUrl) VALUES (NULL,'${newRestaurant.name}','${newRestaurant.type}','${newRestaurant.imgUrl}')`,
     (error, results, fields) => {
         if (error) throw error;
         return true;
     })
 export const getAll = () => sql.query(
-    "SELECT * FROM `se_database.Restaurant` WHERE 1",
+    "SELECT * FROM `Restaurant` WHERE 1",
     (error, results, fields) => {
         if (error) throw error;
     
