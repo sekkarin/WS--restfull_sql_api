8099bda65ee540f3ea7051d5bce9177fe16b070f
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-10-11 16:13:21 +0700

    feat: set head

--- src/index.ts
+++ src/index.ts
@@ -26,32 +26,32 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
-const allowedOrigins = ["http://127.0.0.1:5173", "*"];
+// const allowedOrigins = ["http://127.0.0.1:5173", "*"];
 
 // app.use(
 //   cors({
 //     credentials: true,
-//     // origin: ["*"],
+//     origin: ["*"],
 //     optionsSuccessStatus: 200,
 //   })
 // );
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//   })
+// );
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
