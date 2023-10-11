0395920cc5c9cd2092bfb7fd5ea77327c2271008
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-10-11 16:28:34 +0700

    feat: deploy

--- dist/src/enum/roles.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles[Roles["USER"] = 1001] = "USER";
-    Roles[Roles["ADMIN"] = 2001] = "ADMIN";
-    Roles[Roles["EDITOR"] = 3001] = "EDITOR";
-})(Roles || (exports.Roles = Roles = {}));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import cors from "cors";
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import routers from "./routers";
@@ -10,22 +10,31 @@ import swaggerDocument from "../swagger.json";
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
+app.use(
+  cors({
+    credentials: true,
+    origin: ["*"],
+    optionsSuccessStatus: 200,
+  })
+);
 // app.use(
 //   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
 //     credentials: true,
-//     origin: ["*"],
-//     optionsSuccessStatus: 200,
 //   })
 // );
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
