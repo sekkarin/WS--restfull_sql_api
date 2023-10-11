7a724d8d3d1f92d294ea468dfcb8525b4c2d0330
ศักรินทร์ สิงห์อยู่ <sekkri1234@gmail.com>
2023-10-11 15:47:57 +0700

    feat: fix cors

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,22 @@ import path from "path";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
+import { NextFunction } from "express-serve-static-core";
 
 const app = express();
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 const allowedOrigins = ["http://127.0.0.1:5173", "*"];
 
 // app.use(
@@ -17,20 +31,23 @@ const allowedOrigins = ["http://127.0.0.1:5173", "*"];
 //     optionsSuccessStatus: 200,
 //   })
 // );
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
